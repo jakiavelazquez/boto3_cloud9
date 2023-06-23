5711b96ef54450ad7ae154f69092fa69c663e1e9
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-23 21:47:00 +0000

    create-internet-gateway-attachment

---
+++ VPC/create/create_internet_gateway_attachment.py
@@ -0,0 +1,10 @@
+import boto3
+
+internet_gateway_id = 'igw-06819799e73a8c799'
+vpc_id = 'vpc-00710b48a44be3f3a'
+
+ec2 = boto3.client('ec2')
+
+ec2.attach_internet_gateway(
+    InternetGatewayId=internet_gateway_id,
+    VpcId=vpc_id)
