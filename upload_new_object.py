610593cb24dacdedea7298de8340a0b2c01b7437
Jakia Velazquez <jakiadavis84@gmail.com>
2023-06-23 00:43:28 +0000

    upload-new-object

--- upload_new_object.py
+++ upload_new_object.py
@@ -2,4 +2,7 @@ import boto3
 
 s3 = boto3.client('s3')
 
-s3.put_object(Bucket="jvelazquez-boto3-06202023", Key="test_text_string.txt", Body="Hey, this is a string", ContentType="text/plain")
+s3.put_object(Bucket="jvelazquez-boto3-06202023", 
+                Key="folder/foldera/folder1/test_text_string.txt", 
+                Body="Hey, this is a string", 
+                ContentType="text/plain")
